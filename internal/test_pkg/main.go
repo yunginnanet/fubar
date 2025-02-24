5b9956872fe7e8985af729ca06399b0af941e504
kayos <kayos@tcp.direct>
2025-02-24 03:08:57 -0800

    Testing: Add more realistic test case

---
+++ internal/test_pkg/main.go
@@ -0,0 +1,6 @@
+package test_package
+
+func BadFunc() {
+	asdf := []string{"y", "e", "e", "t"}
+	println(asdf[5])
+}
--- panics_test.go
+++ panics_test.go
@@ -2,7 +2,7 @@ package fubar
 
 import (
 	"errors"
-	"os"
+	test_package "github.com/yunginnanet/fubar/internal/test_pkg"
 	"reflect"
 	"strings"
 	"testing"
@@ -84,31 +84,14 @@ func TestPanicPrint(t *testing.T) {
 	p.Stderr()
 }
 
-func TestName(t *testing.T) {
-	if _, err := os.Stat("./go.mod"); err != nil {
-		t.Skip(err.Error())
-	}
-	var gmn string
-	var need string
-	t.Run("goModName", func(t *testing.T) {
-		need = "github.com/yunginnanet/fubar"
-		if gmn = goModName(); gmn != need {
-			t.Errorf("\ngot: '%s'\nexpected: '%s'", gmn, need)
-		}
-		t.Log(gmn)
-	})
-	t.Run("tail(goModName)", func(t *testing.T) {
-		need = "yunginnanet/fubar"
-		if gmn = tail(gmn); gmn != need {
-			t.Errorf("\ngot: '%s'\nexpected: '%s'", gmn, need)
-		}
-		t.Log(gmn)
-	})
-	t.Run("getName", func(t *testing.T) {
-		need := gmn
-		gmn = getName()
-		if gmn != need {
-			t.Errorf("\ngot: '%s'\nexpected: '%s'", gmn, need)
-		}
+func TestPanicModule(t *testing.T) {
+	t.Run("test_pkg", func(t *testing.T) {
+		defer func() {
+			if r := recover(); r != nil {
+				p := NewPanic(r)
+				p.Stderr()
+			}
+		}()
+		test_package.BadFunc()
 	})
 }
